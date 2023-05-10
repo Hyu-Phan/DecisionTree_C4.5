79ad7bef36db4736431920a466ee8b621ec581cf
Hyu-Phan <pqhuy27122000@gmail.com>
2023-05-10 16:23:45 +0700

    update C4.5

--- C4_5.py
+++ C4_5.py
@@ -2,13 +2,13 @@ import numpy as np
 import pandas as pd
 
 class Node:
-    def __init__(self, isLeaf, label, threshold):
+    def __init__(self, isLeaf, label, threshold, gainRatio=None):
         self.label = label
         self.threshold = threshold
         self.category = []
         self.isLeaf = isLeaf
         self.children = []
-
+        self.gainRatio = gainRatio
 
 class C45:
     def __init__(self):
@@ -34,74 +34,107 @@ class C45:
             return Node(True, majorLabel, None)
         
         else :
-            bestAttribute, bestThreshold, bestPartitions = self.findBestAttribute(data, labels)
+            bestAttribute, bestThreshold, bestPartitions, labelsOfPartition, gainRatio = self.findBestAttribute(data, labels)
             
-            return 
+            node = Node(False, bestAttribute, bestThreshold, gainRatio)
+
+            if bestThreshold is None:
+                for partition in bestPartitions:
+                    node.category.append(partition.iloc[0][bestAttribute])
+            
+            node.children = [self.createNode(partition.drop([bestAttribute], axis=1), labels) for partition, labels in zip(bestPartitions, labelsOfPartition)]
+            return node
 
     def findBestAttribute(self, data, labels):
         splitted = []
         maxGainRatio = -1*float('inf')
         bestAttribute = -1
         threshold = None
+        label_splitted = []
+        best_threshold = None
 
         for attribute in data.columns:
             data = data.sort_values(attribute)
-            if self.isAttrDiscrete(attribute):
+            labels = labels[data.index]
+            if self.isAttrDiscrete(data[attribute]):
                 partitions = []
+                labelsOfPartition = []
+
                 start_index = 0
                 for index in range(len(data) - 1):
-                    if data[attribute][index] != data[attribute][index + 1]:
-                        partitions.append(data.iloc[start_index:index])
+                    if data.iloc[index][attribute] != data.iloc[index + 1][attribute]:
+                        partitions.append(data.iloc[start_index:index+1])
+                        labelsOfPartition.append(labels.iloc[start_index:index+1])
+                        start_index = index+1
                 partitions.append(data.iloc[start_index:])
-                e = self.gainRatio(partitions)
+                labelsOfPartition.append(labels.iloc[start_index:])
+                # partitions = data.groupby(attribute)
+                # labelsOfPartition = labels.groupby(data[attribute])
+                # print(labelsOfPartition.get_group('Overcast'))
+
+                e = self.gainRatio(labels, labelsOfPartition)
                 if e >= maxGainRatio:
                     splitted = partitions
+                    label_splitted = labelsOfPartition
                     maxGainRatio = e
                     bestAttribute = attribute
-                    best_threshold = threshold    
-            else:
+                    best_threshold = None    
+            
+            else:     
                 threshold = []
-                prev = data[attribute][0]
+                threshold_value=[]
                 for i in range(len(data) - 1):
-                    if data[attribute][i] != data[attribute][i + 1] and labels[i] != labels[i + 1]:
-                        threshold.append((data[attribute][i + 1] + prev) / 2)
-                        prev = data[attribute][i + 1]
+                    if data.iloc[i][attribute] != data.iloc[i + 1][attribute]:
+                        threshold_value.append((data.iloc[i][attribute] + data.iloc[i+1][attribute]) / 2)
+                        threshold.append(i+1)
+
                 lenThreshold = len(threshold)
                 partition = 2 
-                limitPartition = 4
+                limitPartition = 3
+
                 while(partition <= limitPartition):
                     indexSplits = self.fillAllTheWaySplit(lenThreshold, partition)
                     for indexSplit in indexSplits:
                         start_index = 0
                         partitions = []
+                        labelsOfPartition = []
                         for index in indexSplit:
-                            partitions.append(data.iloc[start_index:index])
-                            start_index = index
-                        partitions.append(data[start_index:])    
-                        e = self.gainRatio(partitions)
+                            partitions.append(data.iloc[start_index:threshold[index]])
+                            labelsOfPartition.append(labels.iloc[start_index:threshold[index]])
+                            start_index = threshold[index]
+                        partitions.append(data.iloc[start_index:]) 
+                        labelsOfPartition.append(labels.iloc[start_index:])
+
+                        e = self.gainRatio(labels, labelsOfPartition)
                         if e >= maxGainRatio:
                             splitted = partitions
+                            label_splitted = labelsOfPartition
                             maxGainRatio = e
                             bestAttribute = attribute
-                            best_threshold = threshold       
+                            best_threshold = threshold_value     
                     partition += 1     
-        return (bestAttribute,best_threshold,splitted)
+
+        return (bestAttribute, best_threshold, splitted ,label_splitted, maxGainRatio)
                  
 
     def fillAllTheWaySplit(self, lenThreshold, partition):
+
         def Try(currIndexOfThreshold, currPartition):
             if currIndexOfThreshold == lenThreshold:
                 return
+            
             if currPartition == partition:
                 element.append(currIndexOfThreshold)
                 ans.append(element.copy())
                 element.pop()
                 Try(currIndexOfThreshold + 1, currPartition)
+
             if currPartition < partition:
                 element.append(currIndexOfThreshold)
                 Try(currIndexOfThreshold + 1, currPartition + 1)
                 element.pop()
                 Try(currIndexOfThreshold + 1, currPartition)
+                
         ans = []
         element = []
         Try(0, 2)
@@ -109,7 +142,8 @@ class C45:
 
 
     def isAttrDiscrete(self, attrValues):
-        return (attrValues.dtype != 'int64' and attrValues.dtype != 'float')
+        type = attrValues.dtype
+        return (type!= 'int64' and type != 'float')
 
 
     def gainRatio(self, data, partitions):
@@ -121,14 +155,17 @@ class C45:
         return gain_ratio
 
 
-    def gainSplit(self, data, partitions):
-        N = len(data)
-        impurity_before = self.entropy(data[:, -1])
+    def gainSplit(self, labels, labelsOfPartition):
+        N = len(labels)
+        impurity_before = self.entropy(labels)
         impurity_after = 0
-        weights = np.array([len(partition) / N for partition in partitions])
-        for i in range(len(partitions)):
-            impurity_after += weights[i] * self.entropy(partitions[i][:, -1])
-
+        # weights = np.array([len(partition) / N for partition in labelsOfPartition])
+        # for i in range(len(labelsOfPartition)):
+        #     impurity_after += weights[i] * self.entropy(labelsOfPartition[i][:, -1])
+        # partition = labelsOfPartition.get_group('Overcast')
+        # print(partition)
+        for partition in labelsOfPartition:
+            impurity_after += len(partition) / N * self.entropy(partition)
         total_gain = impurity_before - impurity_after
         return total_gain
 
@@ -152,7 +189,7 @@ class C45:
 
     def checkAllSameClass(self, labels):
         if len(np.unique(labels)) == 1:
-            return labels[0]
+            return labels.iloc[0]
         return None
     
 
@@ -171,32 +208,32 @@ class C45:
                 # Categorical
                 for index, child in enumerate(node.children):
                     if child.isLeaf:
-                        print(space + "Attribute {} = {} : {}".format(node.label, node.category[index] , child.label))
+                        print(space + "{} = {} : {}".format(node.label, node.category[index] , child.label))
                     else:
-                        print(space + "Attribute {} = {} :".format(node.label, node.category[index]))
-                        self.printNode(child, space + "  ")
+                        print(space + "{} = {} :".format(node.label, node.category[index]))
+                        self.printNode(child, space + "     ")
             else:
                 for index, child in enumerate(node.children):
                     if index == 0:
                         if child.isLeaf:
-                            print(space + "Attribute {} <= {} : {}".format(node.label, node.threshold[index] , child.label))
+                            print(space + "{} <= {} : {}".format(node.label, node.threshold[index] , child.label))
                         else:
-                            print(space + "Attribute {} <= {} :".format(node.label, node.threshold[index]))
-                            self.printNode(child, space + "  ")
+                            print(space + "{} <= {} :".format(node.label, node.threshold[index]))
+                            self.printNode(child, space + "     ")
 
                     elif index == len(node.children) - 1:
                         if child.isLeaf:
-                            print(space + "Attribute {} > {} : {}".format(node.label, node.threshold[index - 1] , child.label))
+                            print(space + "{} > {} : {}".format(node.label, node.threshold[index - 1] , child.label))
                         else:
-                            print(space + "Attribute {} > {} :".format(node.label, node.threshold[index - 1]))
-                            self.printNode(child, space + "  ")
+                            print(space + "{} > {} :".format(node.label, node.threshold[index - 1]))
+                            self.printNode(child, space + "     ")
 
                     else:
                         if child.isLeaf:
-                            print(space + "{} < Attribute {} <= {} : {}".format(node.threshold[index - 1], node.label, node.threshold[index] , child.label))
+                            print(space + "{} < {} <= {} : {}".format(node.threshold[index - 1], node.label, node.threshold[index] , child.label))
                         else:
-                            print(space + "{} < Attribute {} <= {} :".format(node.threshold[index - 1], node.label, node.threshold[index]))
-                            self.printNode(child, space + "  ")
+                            print(space + "{} < {} <= {} :".format(node.threshold[index - 1], node.label, node.threshold[index]))
+                            self.printNode(child, space + "     ")
 
 
     def predict(self, data):
